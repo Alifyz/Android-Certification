369d4b20db74ccb1aea0f1b7817c514bbf4b0fcf
alifyz <alifyz@outlook.com>
2019-01-13 18:58:36 -0200

    add: new ViewModel example.

---
+++ code_samples/arch-components/ViewModelExample/app/src/main/java/com/alifyz/www/viewmodelexample/CounterViewModel.kt
@@ -0,0 +1,11 @@
+package com.alifyz.www.viewmodelexample
+
+import android.arch.lifecycle.ViewModel
+
+class CounterViewModel : ViewModel() {
+
+    //Objetos e Informações que Precisamos reter
+    //Durante o Ciclo de Vida
+    var counterNumber = 0
+
+}
--- code_samples/arch-components/ViewModelExample/app/src/main/java/com/alifyz/www/viewmodelexample/Fragment2.kt
+++ code_samples/arch-components/ViewModelExample/app/src/main/java/com/alifyz/www/viewmodelexample/Fragment2.kt
@@ -1,5 +1,6 @@
 package com.alifyz.www.viewmodelexample
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -9,24 +10,28 @@ import kotlinx.android.synthetic.main.fragment_layout.*
 
 class Fragment2 : Fragment() {
 
-    var counterNumber : Int = 0
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_layout, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        texto.text = counterNumber.toString()
+        //Criação de uma ViewModel usando um ViewModelProvider
+        val viewModel = ViewModelProviders.of(this).get(CounterViewModel::class.java)
+
+        //Extraindo os Dados da ViewModel
+        texto.text = viewModel.counterNumber.toString()
 
         aumentar.setOnClickListener {
-            counterNumber+= 1
-            texto.text = counterNumber.toString()
+            //Salvando o novo Estado do Contador - ViewModel
+            viewModel.counterNumber+= 1
+            texto.text = viewModel.counterNumber.toString() //Acessando Diretamente da ViewModel
         }
 
         diminuir.setOnClickListener {
-            counterNumber-= 1
-            texto.text = counterNumber.toString()
+            //Salvando o novo Estado do Contador - ViewModel
+            viewModel.counterNumber-= 1
+            texto.text = viewModel.counterNumber.toString() //Acessando Diretamente da ViewModel
         }
     }
 }
